e9bc9878487efcf4d45864848cc5ad3b4d1a3f60
awhiting18 <awhiting@unomaha.edu>
2022-03-17 12:21:43 -0500

    Changes: -Added redirection -changed file structure for redirection

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Pusher from 'pusher-js'
+import config from '../config.json'
 
-export default function Home({ handleLoginChange, handleLogin }) {
+function Home() {
+  const [gameCode, setGameCode] = useState('')
+  const router = useRouter()
+
+  /************Pusher Variables*************/
+  let channel
+  let pusher
+
+  /***********FUNCTIONS**********/
+  const redirect = () => {
+    router.push({ pathname: '/controller', query: { gameCode: gameCode } })
+  }
+
+  const connect = () => {
+    if (gameCode.length != 5) {
+      console.log('Gamecode: ', gameCode)
+      alert('Please enter a valid game code.')
+      return
+    } else {
+      pusher = new Pusher(config.key, {
+        cluster: 'us2',
+        authEndpoint: 'api/pusher/auth',
+      })
+      channel = pusher.subscribe('private-pong' + gameCode)
+      channel.bind('pusher:subscription_succeeded', () => {
+        let triggered = channel.trigger('client-controllerconnect', 'Connected')
+        redirect()
+      })
+    }
+  }
+
+  /**************DISPLAY***********/
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Enter Player Name..."
-          onChange={handleLoginChange}
-          className="jpx-2 rounded-md border-2 border-gray-500 py-2"
-        />
-        <button className="ml-2 rounded-md bg-purple-500 py-2 px-2 text-white">
-          Submit
-        </button>
-      </form>
+      <input
+        type="text"
+        placeholder="Enter game code"
+        onChange={(e) => {
+          setGameCode(e.currentTarget.value)
+        }}
+        className="rounded-md border-2 border-gray-500 py-2"
+      />
+      <button
+        className="ml-3 rounded-md bg-blue-500 py-2 px-2 text-white"
+        onClick={() => connect()}
+      >
+        Connect
+      </button>
     </div>
   )
 }
+
+export default Home
