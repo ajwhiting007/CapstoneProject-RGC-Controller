2416b3ef07383d2d4eceede200366d01c22ebb7b
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2022-03-18 08:14:20 -0500

    Trying flexbox css with controller

--- pages/controller.js
+++ pages/controller.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Pusher from 'pusher-js'
 import config from '../config.json'
+import '../styles/controller.css'
 
 export default function Controller() {
   /**********Router Variables************/
@@ -35,12 +36,12 @@ export default function Controller() {
 
   /**********Display************/
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div id="container" className="flex min-h-screen flex-col items-center justify-center py-2">
       <div>
-        <button onClick={() => triggerEvent('up')}>Send Up</button>
+        <button id="item" onClick={() => triggerEvent('up')}>Send Up</button>
       </div>
       <div>
-        <button onClick={() => triggerEvent('down')}>Send Down</button>
+        <button id="item" onClick={() => triggerEvent('down')}>Send Down</button>
       </div>
     </div>
   )
