7947d9de5f6b9f9e668e3308913518dc1fb3e9c0
awhiting18 <awhiting@unomaha.edu>
2022-03-22 11:13:28 -0500

    made changes to allow for the game to be played on the game project

--- pages/controller.js
+++ pages/controller.js
@@ -11,10 +11,12 @@ export default function Controller() {
   /**********Pusher Variables************/
   let pusher
   let channel
+  const [channelTest, setChannelTest] = useState(null)
 
   /**********On Screen Load Functions************/
   useEffect(() => {
     onScreenLoad()
+    console.log(channel)
   }, [])
   const onScreenLoad = () => {
     //Creating the pusher to listen in for the redirection signal
@@ -23,56 +25,89 @@ export default function Controller() {
       authEndpoint: 'api/pusher/auth',
     })
     channel = pusher.subscribe('private-pong' + gameCode)
+    setChannelTest(channel)
     channel.bind('pusher:subscription_succeeded', () => {
-      let triggered = channel.trigger('client-controllerconnect', 'Connected')
+      channel.trigger('client-controllerconnect', 'Connected')
     })
   }
 
   /************Functions*************/
   const triggerEvent = (move) => {
-    channel.trigger('client-controllermovement', move)
+    channelTest.trigger('client-controllermovement', move)
   }
 
   /**********Display************/
   return (
-    <div className="container" >
+    <div className="container">
       <div className="spacing">
-        <button onClick={() => triggerEvent('up')}><SVGArrow direction="up" />Send Up</button>
+        <button onClick={() => triggerEvent('1')}>
+          <SVGArrow direction="up" />
+          Send Up
+        </button>
       </div>
-      <div className="spacing"> 
-        <button onClick={() => triggerEvent('down')}><SVGArrow direction="down" />Send Down</button>
+      <div className="spacing">
+        <button onClick={() => triggerEvent('-1')}>
+          <SVGArrow direction="down" />
+          Send Down
+        </button>
       </div>
     </div>
   )
 }
 
 function SVGArrow(props) {
-  if(props.direction == "up")
-  {
-    return <svg className='upButton' width="90%" height="90%" version="1.1" viewBox="0 0 33.798 34.279" xmlns="http://www.w3.org/2000/svg">
-     <g transform="translate(-84.137 -120.7)">
-      <path d="m117.55 138.12a16.51 16.51 0 0 1-16.51 16.51 16.51 16.51 0 0 1-16.51-16.51 16.51 16.51 0 0 1 16.51-16.51 16.51 16.51 0 0 1 16.51 16.51z" fill-rule="evenodd" stroke="#000" stroke-width=".04827"/>
-      <g fill="#97b4d4" stroke="#fff" stroke-width=".6">
-       <path d="m97.664 126h6.1819v6.1819h-6.1819z"/>
-       <path d="m97.664 132.19h6.1819v6.1819h-6.1819z"/>
-       <path d="m97.664 138.37h6.1819v6.1819h-6.1819z"/>
-       <path d="m103.85 144.64h4.8271c-2.4306 2.2633-5.1108 4.2306-7.7233 6.2752-2.7674-1.8939-5.2078-4.0741-7.7233-6.2752h4.3444z"/>
-      </g>
-     </g>
-    </svg>;
+  if (props.direction == 'up') {
+    return (
+      <svg
+        className="upButton"
+        width="90%"
+        height="90%"
+        version="1.1"
+        viewBox="0 0 33.798 34.279"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g transform="translate(-84.137 -120.7)">
+          <path
+            d="m117.55 138.12a16.51 16.51 0 0 1-16.51 16.51 16.51 16.51 0 0 1-16.51-16.51 16.51 16.51 0 0 1 16.51-16.51 16.51 16.51 0 0 1 16.51 16.51z"
+            fill-rule="evenodd"
+            stroke="#000"
+            stroke-width=".04827"
+          />
+          <g fill="#97b4d4" stroke="#fff" stroke-width=".6">
+            <path d="m97.664 126h6.1819v6.1819h-6.1819z" />
+            <path d="m97.664 132.19h6.1819v6.1819h-6.1819z" />
+            <path d="m97.664 138.37h6.1819v6.1819h-6.1819z" />
+            <path d="m103.85 144.64h4.8271c-2.4306 2.2633-5.1108 4.2306-7.7233 6.2752-2.7674-1.8939-5.2078-4.0741-7.7233-6.2752h4.3444z" />
+          </g>
+        </g>
+      </svg>
+    )
   }
-  if(props.direction == "down")
-  {
-    return <svg className='downButton' width="90%" height="90%" version="1.1" viewBox="0 0 33.798 34.279" xmlns="http://www.w3.org/2000/svg">
-     <g transform="translate(-84.137 -120.7)">
-      <path d="m117.55 138.12a16.51 16.51 0 0 1-16.51 16.51 16.51 16.51 0 0 1-16.51-16.51 16.51 16.51 0 0 1 16.51-16.51 16.51 16.51 0 0 1 16.51 16.51z" fill-rule="evenodd" stroke="#000" stroke-width=".04827"/>
-      <g fill="#97b4d4" stroke="#fff" stroke-width=".6">
-       <path d="m97.664 126h6.1819v6.1819h-6.1819z"/>
-       <path d="m97.664 132.19h6.1819v6.1819h-6.1819z"/>
-       <path d="m97.664 138.37h6.1819v6.1819h-6.1819z"/>
-       <path d="m103.85 144.64h4.8271c-2.4306 2.2633-5.1108 4.2306-7.7233 6.2752-2.7674-1.8939-5.2078-4.0741-7.7233-6.2752h4.3444z"/>
-      </g>
-     </g>
-    </svg>;
+  if (props.direction == 'down') {
+    return (
+      <svg
+        className="downButton"
+        width="90%"
+        height="90%"
+        version="1.1"
+        viewBox="0 0 33.798 34.279"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g transform="translate(-84.137 -120.7)">
+          <path
+            d="m117.55 138.12a16.51 16.51 0 0 1-16.51 16.51 16.51 16.51 0 0 1-16.51-16.51 16.51 16.51 0 0 1 16.51-16.51 16.51 16.51 0 0 1 16.51 16.51z"
+            fill-rule="evenodd"
+            stroke="#000"
+            stroke-width=".04827"
+          />
+          <g fill="#97b4d4" stroke="#fff" stroke-width=".6">
+            <path d="m97.664 126h6.1819v6.1819h-6.1819z" />
+            <path d="m97.664 132.19h6.1819v6.1819h-6.1819z" />
+            <path d="m97.664 138.37h6.1819v6.1819h-6.1819z" />
+            <path d="m103.85 144.64h4.8271c-2.4306 2.2633-5.1108 4.2306-7.7233 6.2752-2.7674-1.8939-5.2078-4.0741-7.7233-6.2752h4.3444z" />
+          </g>
+        </g>
+      </svg>
+    )
   }
 }
