8840e46f7d2f37413dc32ef4d859152ec612bc28
awhiting18 <awhiting@unomaha.edu>
2022-05-02 11:15:00 -0500

    added more automated testing

---
+++ Tests/timeout_notyping.js
@@ -0,0 +1,26 @@
+const LoginTester = require('./Utility-Function/Login')
+console.log(typeof LoginTester, LoginTester)
+const { Builder, By, Key } = require('selenium-webdriver')
+;(async function myFunction() {
+  let driver = await new Builder().forBrowser('chrome').build()
+
+  let logintest = LoginTester(driver)
+  let gameCode = 'MCGYS'
+
+  let afterLogin = async function (driver) {}
+
+  logintest.then(() => {
+    afterLogin(driver)
+  })
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+  await sleep(5000)
+  driver.findElement(By.xpath('//input')).sendKeys(gameCode)
+  await sleep(5000)
+  driver.findElement(By.xpath('//button[contains(text(), "Connect")]')).click()
+  await sleep(5000)
+  driver.findElement(By.xpath('//div[contains(text(), "×")]')).click()
+  await sleep(5000)
+})()
